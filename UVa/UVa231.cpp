ae2db5432542ff81ee5652654172d59abed8970a
Jose Luis Gallegos <jose.gallegos@cimat.mx>
2018-07-31 22:40:48 -0500

    Added some DP solutions.

---
+++ UVa/UVa231.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/priority_queue.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+typedef pair<ii, int> tern;
+typedef pair<ii, ii> quad;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<tern> vtern;
+typedef vector<quad> vquad;
+// minHeap, BinomialHeap and FibonacciHeap for later use, policy based data structures
+template <class T> using minHeap = __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag>;
+template <class T> using minBinHeap = __gnu_pbds::priority_queue<T, greater<T>, rc_binomial_heap_tag>;
+template <class T> using minFHeap = __gnu_pbds::priority_queue<T, greater<T>, thin_heap_tag>;
+template <class T> using maxFHeap = __gnu_pbds::priority_queue<T, less<T>, thin_heap_tag>;
+template <class T> using maxBinHeap = __gnu_pbds::priority_queue<T, less<T>, rc_binomial_heap_tag>;
+// ordered set and map with policy, policy based data structures
+template <class T> using oSet = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T, class U> using oMap = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// patricia trie, policy based data structure
+typedef trie<string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> Trie;
+// constants
+const int INF = (int) 1e9 + 7;
+const ll LLINF = (ll) 4e18 + 7;
+const double pi = acos(-1.0);
+// easy access/use
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define view(x) cout << #x << ": " << x << endl;
+#define sz(c) (int)((c).size())
+#define all(c) (c).begin(), (c).end()
+#define justN(c, n) (c).begin(), (c).begin() + n
+#define sq(a) (a) * (a)
+#define fi first
+#define se second
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
+#define FOR(i, start, end) for( int i(start); i < (end); ++ i )
+#define REP(i, end) FOR(i, 0, end)
+#define FORD(i, start, end) for( int i(start); i >= end; -- i )
+#define gcd(a, b) __gcd(a, b)
+// bit related builtin functions
+#define idxLSB(x) __builtin_ffs(x)
+#define idxLSBl(x) __builtin_ffsl(x)
+#define idxLSBll(x) __builtin_ffsll(x)
+#define leftZeroBits(x) __builtin_clz(x)
+#define leftZeroBitsl(x) __builtin_clzl(x)
+#define leftZeroBitsll(x) __builtin_clzll(x)
+#define rightZeroBits(x) __builtin_ctz(x)
+#define rightZeroBitsl(x) __builtin_ctzl(x)
+#define rightZeroBitsll(x) __builtin_ctzll(x)
+#define cntSetBits(x) __builtin_popcount(x)
+#define cntSetBitsl(x) __builtin_popcountl(x)
+#define cntSetBitsll(x) __builtin_popcountll(x)
+
+int main(void){
+  int n, i = 0, tc = 1, ans = 0, prev = 0;
+  fastio;
+  vector<int> heights( 1000000, 0 ), values( 1000000, 0 ), dp( 1000000, 0 ), values_id( 1000000, 0 );
+  while( cin >> n ){
+    if( prev == -1 and n == -1 ) break;
+    if( n == -1 ){
+      /* L(M)IS (Longest Monotonic Increasing Subsequence) */
+      int lisB = 0, lisE = 0;
+      FOR( j, 0, i ){
+	int pos = upper_bound( justN( values, lisB ), heights[j] ) - values.begin();
+	values[pos] = heights[j];
+	values_id[pos] = j; // you can get back the sequence with this
+	dp[j] = pos ? values_id[pos - 1] : -1; // and this
+	if( pos + 1 >= lisB ) lisB = pos + 1, lisE = j; // and lisE and a stack
+      }
+      ans = lisB;
+      if( ans > 0 ) cout << "Test #" << tc ++ << ":\n  maximum possible interceptions: " << ans << '\n';
+      i = 0;
+      prev = n;
+    }
+    else heights[i++] = -n, cout << (prev == -1 ? "\n" : ""), prev = n;
+  }
+  return 0;
+}
