4a94b11dbe8903550ad0dadf174ac8ace16006c6
jocagos <zagerer@gmail.com>
2018-10-24 16:39:51 -0500

    The Seasonal War and Graph Connectivity (2 solutions)

---
+++ UVa/UVa459-UFDS.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/priority_queue.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long i64;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef pair<double, double> dd;
+typedef pair<ii, int> tern;
+typedef pair<ii, ii> quad;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ii> vii;
+typedef vector<dd> vdd;
+typedef vector<tern> vtern;
+typedef vector<quad> vquad;
+// minHeap, BinomialHeap and FibonacciHeap for later use, policy based data structures
+template <class T> using minHeap = __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag>;
+template <class T> using minBinHeap = __gnu_pbds::priority_queue<T, greater<T>, rc_binomial_heap_tag>;
+template <class T> using minFHeap = __gnu_pbds::priority_queue<T, greater<T>, thin_heap_tag>;
+template <class T> using maxFHeap = __gnu_pbds::priority_queue<T, less<T>, thin_heap_tag>;
+template <class T> using maxBinHeap = __gnu_pbds::priority_queue<T, less<T>, rc_binomial_heap_tag>;
+// ordered set and map with policy, policy based data structures
+template <class T> using oSet = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T, class U> using oMap = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// patricia trie, policy based data structure
+typedef trie<string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> Trie;
+// constants
+const int INF = (int) 1e9 + 7;
+const ll LLINF = (ll) 4e18 + 7;
+const double pi = acos(-1.0);
+// /* slaps vector */ This bad boy can hold SO MANY
+// values to compare a value to!
+template<typename T>
+bool isIn( T const &value, std::vector<T>& v ){
+  return std::find( v.begin(), v.end(), value ) != v.end();
+}
+// /* slaps initializer_list */ And THIS bad boy can hold
+// ANY initializer_list with the same type as the value
+// to look forward!
+template<typename T>
+bool isIn( T const &value, std::initializer_list<T> v ){
+  return std::find( v.begin(), v.end(), value ) != v.end();
+}
+
+// Took it from CodeForces, great hash for anti-hacking maps and sets
+struct myHash {
+  static i64 splitmix64( i64 x ) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()( i64 x ) const {
+    static const i64 FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+
+// easy access/use
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define view(x) cout << #x << ": " << x << endl;
+#define sz(c) (int)((c).size())
+#define all(c) (c).begin(), (c).end()
+#define in( a, b, x ) ( (a) <= (x) and (x) <= (b) )
+#define justN(c, n) (c).begin(), (c).begin() + n
+#define sq(a) (a) * (a)
+#define fi first
+#define se second
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
+#define FOR(i, start, end) for( int i(start); i < (end); ++ i )
+#define REP(i, end) FOR(i, 0, end)
+#define FORD(i, start, end) for( int i(start); i >= end; -- i )
+#define gcd(a, b) __gcd(a, b)
+// bit related builtin functions
+#define idxLSB(x) __builtin_ffs(x)
+#define idxLSBl(x) __builtin_ffsl(x)
+#define idxLSBll(x) __builtin_ffsll(x)
+#define leftZeroBits(x) __builtin_clz(x)
+#define leftZeroBitsl(x) __builtin_clzl(x)
+#define leftZeroBitsll(x) __builtin_clzll(x)
+#define rightZeroBits(x) __builtin_ctz(x)
+#define rightZeroBitsl(x) __builtin_ctzl(x)
+#define rightZeroBitsll(x) __builtin_ctzll(x)
+#define cntSetBits(x) __builtin_popcount(x)
+#define cntSetBitsl(x) __builtin_popcountl(x)
+#define cntSetBitsll(x) __builtin_popcountll(x)
+constexpr int MAXN = 26;
+
+unordered_map<char, int> c2i { {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4}, {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9}, {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14}, {'P', 15}, {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19}, {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25} };
+
+class UnionFind {
+private:
+  vi p, rank, setSize;
+  int numSets;
+public:
+  UnionFind(int N) {
+    setSize.assign(N, 1); numSets = N; rank.assign(N, 0);
+    p.assign(N, 0); for (int i = 0; i < N; i++) p[i] = i; }
+  int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+  bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+  void unionSet(int i, int j) { 
+    if (!isSameSet(i, j)) { numSets--; 
+    int x = findSet(i), y = findSet(j);
+    if (rank[x] > rank[y]) { p[y] = x; setSize[x] += setSize[y]; }
+    else                   { p[x] = y; setSize[y] += setSize[x];
+                             if (rank[x] == rank[y]) rank[y]++; } } }
+  int numDisjointSets() { return numSets; }
+  int sizeOfSet(int i) { return setSize[findSet(i)]; }
+};
+
+int main(void){
+  int tc;
+  fastio;
+  cin >> tc;
+  string line;
+  getline( cin, line ); // Get line after test cases
+  getline( cin, line ); // enter dummy line
+  while( tc -- ){
+    int n = 0;
+    getline( cin, line );
+    n = c2i[line[0]] + 1;
+    UnionFind ufds( n );
+    while( getline( cin, line ) ){
+      if( line == "" ) break;
+      ufds.unionSet( c2i[line[0]], c2i[line[1]] );
+    }
+    cout << ufds.numDisjointSets() << "\n";
+    if( tc ) cout << "\n";
+  }
+  return 0;
+}
