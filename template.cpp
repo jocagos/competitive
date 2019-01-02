a81deaab70c6be3782271986b0fd68cf3cbf3427
jocagos <zagerer@gmail.com>
2019-01-02 00:47:54 -0600

    Updated template

--- template.cpp
+++ template.cpp
@@ -6,22 +6,33 @@
 
 using namespace std;
 using namespace __gnu_pbds;
+using ll = long long;
+using i64 = unsigned long long;
+using ld = long double;
+// using ii = tuple<int, int>; // or pair<int, int>
+// using dd = tuple<double, double>; // or pair< double, double >
+template <class T> using twin = tuple<T, T>;
+template <class T> using triad = tuple<T, T, T>;
+template <class T> using quad = tuple<T, T, T, T>;
+using vi = vector<int>;
+using vs = vector<string>;
+using vd = vector<double>;
+using vld = vector<ld>;
+template <class T, class U = T>
+using vtwin = vector<tuple<T, U>>;
+template <class T, class U = T, class V = T>
+using vtriad = vector<tuple<T, U, V>>;
+template <class T, class U = T, class V = T, class W = T>
+using vquad = vector<tuple<T, U, V, W>>;
 
-typedef long long ll;
-typedef unsigned long long i64;
-typedef long double ld;
-typedef pair<int, int> ii;
-typedef pair<double, double> dd;
-typedef pair<ii, int> tern;
-typedef pair<ii, ii> quad;
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef vector<double> vd;
-typedef vector<ii> vii;
-typedef vector<dd> vdd;
-typedef vector<tern> vtern;
-typedef vector<quad> vquad;
 // minHeap, BinomialHeap and FibonacciHeap for later use, policy based data structures
+/* NOTES:
+`pairing_heap_tag` is good for non-primitive data types like `string`
+`binary_heap_tag` is good for primitive data types
+`binomial_heap_tag` is good for responsiveness
+`rc_binomial_heap_tag` has more responsiveness at the cost of worse constants
+`thin_heap_tag` is good for graph algorithms (FibonacciHeap)
+ */
 template <class T> using minHeap = __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag>;
 template <class T> using minBinHeap = __gnu_pbds::priority_queue<T, greater<T>, rc_binomial_heap_tag>;
 template <class T> using minFHeap = __gnu_pbds::priority_queue<T, greater<T>, thin_heap_tag>;
@@ -36,7 +47,7 @@ typedef trie<string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_
 const int INF = (int) 1e9 + 7;
 const ll LLINF = (ll) 4e18 + 7;
 const double pi = acos(-1.0);
-constexpr ii n8[8] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } }, n4[4] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
+constexpr twin<int> n8[8] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } }, n4[4] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
 constexpr bool V = true, NV = false;
 // /* slaps vector */ This bad boy can hold SO MANY
 // values to compare a value to!
@@ -67,41 +78,6 @@ struct myHash {
   }
 };
 
-// easy access/use
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define prepare( cont, n ) (cont).clear(); (cont).resize( n )
-#define viewStr(s) cout << #s << ": >" << (s) << "<" << endl
-#define view(x) cout << #x << ": " << x << endl
-#define sz(c) (int)((c).size())
-#define all(c) (c).begin(), (c).end()
-#define in( a, b, x ) ( (a) <= (x) and (x) <= (b) )
-#define justN(c, n) (c).begin(), (c).begin() + n
-#define sq(a) (a) * (a)
-#define fi first
-#define se second
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
-#define MT make_tuple
-#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
-#define FOR(i, start, end) for( int i(start); i < (end); ++ i )
-#define REP(i, end) FOR(i, 0, end)
-#define FORD(i, start, end) for( int i(start); i >= end; -- i )
-#define gcd(a, b) __gcd(a, b)
-// bit related builtin functions
-#define idxLSB(x) __builtin_ffs(x)
-#define idxLSBl(x) __builtin_ffsl(x)
-#define idxLSBll(x) __builtin_ffsll(x)
-#define leftZeroBits(x) __builtin_clz(x)
-#define leftZeroBitsl(x) __builtin_clzl(x)
-#define leftZeroBitsll(x) __builtin_clzll(x)
-#define rightZeroBits(x) __builtin_ctz(x)
-#define rightZeroBitsl(x) __builtin_ctzl(x)
-#define rightZeroBitsll(x) __builtin_ctzll(x)
-#define cntSetBits(x) __builtin_popcount(x)
-#define cntSetBitsl(x) __builtin_popcountl(x)
-#define cntSetBitsll(x) __builtin_popcountll(x)
-constexpr int MAXN = 0; // modify
 /* FAST I/O for integers
  * USE: just add the characters to `buf` and check if you are close to overflow
  * and if so just print `buf` then reset iterator to buf.
@@ -130,6 +106,51 @@ int print_int( int N, int idx, int nd = ZERO ){
   return idx;
 }
  */
+
+// can't remove these macros
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define viewStr(s) cout << #s << ": >" << (s) << "<" << endl
+#define view(x) cout << #x << ": " << x << endl
+#define sz(c) (int)((c).size())
+#define all(c) (c).begin(), (c).end()
+// pretty useful though hard to make a template out of it that works
+// with almost any type without casts and so
+// it might work:
+/*
+  template <typename T, typename U, typename V>
+  inline bool in( T a, U b, V x ){
+    return a <= x and x <= b;
+  }
+ */
+// easy access/use
+#define in( a, b, x ) ( (a) <= (x) and (x) <= (b) )
+#define justN(c, n) ( (c).begin(), (c).begin() + n )
+#define sq(a) (a) * (a)
+#define fi first
+#define se second
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
+#define FOR(i, start, end) for( int i(start); i < (end); ++ i )
+#define REP(i, end) FOR(i, 0, end)
+#define FORD(i, start, end) for( int i(start); i >= end; -- i )
+#define gcd(a, b) __gcd(a, b)
+// bit related builtin functions
+#define idxLSB(x) __builtin_ffs(x)
+#define idxLSBl(x) __builtin_ffsl(x)
+#define idxLSBll(x) __builtin_ffsll(x)
+#define leftZeroBits(x) __builtin_clz(x)
+#define leftZeroBitsl(x) __builtin_clzl(x)
+#define leftZeroBitsll(x) __builtin_clzll(x)
+#define rightZeroBits(x) __builtin_ctz(x)
+#define rightZeroBitsl(x) __builtin_ctzl(x)
+#define rightZeroBitsll(x) __builtin_ctzll(x)
+#define cntSetBits(x) __builtin_popcount(x)
+#define cntSetBitsl(x) __builtin_popcountl(x)
+#define cntSetBitsll(x) __builtin_popcountll(x)
+constexpr int MAXN = 0; // modify
 
 int main(void){
   int n;
