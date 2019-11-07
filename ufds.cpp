90ea4b179646860edf26e7f09100e7e6915b35f9
jocagos <zagerer@gmail.com>
2019-11-06 21:37:22 -0600

    Added UFDS

---
+++ ufds.cpp
@@ -0,0 +1,33 @@
+constexpr int MAXSIZE = 1000001;
+int pa[MAXSIZE], ra[MAXSIZE], set_size[MAXSIZE];
+int num_sets = MAXSIZE;
+
+void init( int n, int idx = 0 ){
+  num_sets = n + idx;
+  for( int i = 0; i < num_sets; ++ i ){
+    pa[i] = i;
+    ra[i] = set_size[i] = 1;
+  }
+}
+
+int find_set( int i ){ return pa[i] == i ? i : pa[i] = find_set( pa[i] ); }
+bool is_same_set( int i, int j ){ return find_set( i ) == find_set( j ); }
+bool union_set( int i, int j ){
+  if( not is_same_set( i, j ) ){
+    num_sets --;
+    int x = find_set( i ), y = find_set( j );
+    if( ra[x] > ra[y] ){
+      pa[y] = x;
+      set_size[x] += set_size[y];
+    }
+    else{
+      pa[x] = y;
+      set_size[y] += set_size[x];
+      if( ra[x] == ra[y] ) ra[y] ++;
+    }
+    return true;
+  }
+  return false;
+}
+
+int size_of_set( int i ){ return set_size[find_set( i )]; }
